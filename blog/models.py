e09503f42e21a6bafe810f42efc8954135023bc1
BruniRj <brunirj729@gmail.com>
2023-03-11 10:12:19 +0300

    First commit project photo_blog

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.db import models
+from PIL import Image
+
+
+class Photo(models.Model):
+    image = models.ImageField(verbose_name='image')
+    caption = models.CharField(max_length=128, blank=True, verbose_name='légende')
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    IMAGE_MAX_SIZE = (800,800)
+    
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
+
+class Blog(models.Model):
+    photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
+    title = models.CharField(max_length=128, verbose_name='titre')
+    content = models.CharField(max_length=5000, verbose_name='contenu')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    starred = models.BooleanField(default=False)
